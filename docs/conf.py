63ca46d68f74067efb2241a82843491f61ae094d
W4termy <wangmingyang21@huawei.com>
2026-01-26 11:34:31 +0800

    feat(docs): Requirements of html
    Co-authored-by: W4termy<wangmingyang21@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !633 merge ht into master
    
    feat(docs): Requirements of html
    
    Created-by: W4termy
    Commit-by: W4termy
    Merged-by: cann-robot
    Description: 
    ```
    新增构建html必要的conf文件以及对一些md文件索引写法的修改
    ```
    
    
    See merge request: cann/pypto!633

---
+++ docs/conf.py
@@ -0,0 +1,71 @@
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 - 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+import os
+import sys
+
+# 将项目根目录加入 Python 路径
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- General configuration ------------------------------------------------
+# 避免构建失败的虚拟导入
+autodoc_mock_imports = ["torch"]
+
+# 项目基本信息
+project = "PyPTO" 
+html_title = "PyPTO文档"
+copyright = "2025-2026 Huawei Technologies Co., Ltd. All Rights Reserved."
+
+# 指定主文档
+master_doc = 'index'
+
+# 扩展列表
+extensions = [
+    "sphinx_tabs.tabs",          # 标签页
+    "sphinx_toolbox.collapse",   # 折叠块
+    "sphinxcontrib.httpdomain",  # HTTP 接口文档
+    "sphinx.ext.napoleon",       # Docstring 解析
+    "sphinx.ext.intersphinx",    # 跨文档链接
+    "sphinx_reredirects",        # 页面重定向
+    "sphinx.ext.mathjax",        # 数学公式
+    "myst_parser",               # Markdown 解析
+    "sphinx.ext.viewcode",       # 显示代码源文件链接
+]
+
+#source_suffix 配置
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# Myst 扩展配置
+myst_enable_extensions = [
+    "colon_fence",  # 支持 ``` 代码块
+    "deflist",      # 支持定义列表
+]
+
+# 语言改为中文
+language = "zh_CN"
+
+# 排除目录
+exclude_patterns = ["_build", "README.md"]
+
+# 代码高亮风格
+pygments_style = "sphinx"
+
+# -- Options for HTML output ----------------------------------------------
+# Furo 主题配置
+html_theme = "furo"
+
+html_theme_options = {
+    "source_edit_link": "https://gitcode.com/cann/pypto/blob/master/docs/{filename}"
+}
+
+# 模板路径
+templates_path = []
