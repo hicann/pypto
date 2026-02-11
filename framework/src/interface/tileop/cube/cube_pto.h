9f07231d6ba4aed73611137c43cfb40ad4123019
wang-zhiyue2 <wangzhiyue2@huawei.com>
2026-02-11 14:17:10 +0800

    fix(operation): Fix gather in l1 performance
    Co-authored-by: wang-zhiyue2<wangzhiyue2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1013 merge gatherFix into master
    
    fix(operation): Fix gather in l1 performance
    
    Created-by: wang-zhiyue2
    Commit-by: wang-zhiyue2
    Merged-by: cann-robot
    Description: 修复pto gather in l1导致的性能下降问题：
    1. 添加gather in l1在内轴32B对齐时，使用ND2ND功能以提升性能。
    
    See merge request: cann/pypto!1013

--- framework/src/interface/tileop/cube/cube_pto.h
+++ framework/src/interface/tileop/cube/cube_pto.h
@@ -656,6 +656,24 @@ TILEOP void TStore(T &dst, U &src, const Coord &coord)
     }
 }
 
+template <int64_t blockSize, typename globalData, typename tileData, typename DstT, typename SrcT, typename BlockT, typename OffsetT>
+INLINE void GatherExecute(DstT dst, SrcT src, BlockT block, OffsetT offset, uint64_t offsetsStartOffset,
+    uint64_t srcColumnStartOffset, uint64_t GMBlockTableOffset, int64_t srcShape0, int64_t srcShape1, 
+    int64_t srcStride0, int64_t srcStride1, int64_t dstShape0, int64_t dstShape1, int64_t srcCol, int64_t loop,
+    int64_t c0Size) {
+    for (int64_t i = 0; i < loop; i++) {
+        uint64_t gatherOffset = offset.GetAddr()[i + offsetsStartOffset];
+        gatherOffset = CalaOffset2PageAttention<uint64_t, typename BlockT::Type, blockSize>(
+            block.GetAddr() + GMBlockTableOffset, gatherOffset);
+        globalData src0Global(
+            (__gm__ typename SrcT::Type *)(src.GetAddr() + gatherOffset * srcCol + srcColumnStartOffset),
+            pto::Shape<1, 1, 1, -1, -1>(srcShape0, srcShape1), pto::Stride<1, 1, 1, -1, -1>(srcStride0, srcStride1));
+        tileData dstL1(dstShape0, dstShape1);
+        pto::TASSIGN(dstL1, (uint64_t)((__cbuf__ typename DstT::Type *)dst.GetAddr() + i * c0Size));
+        pto::TLOAD(dstL1, src0Global);
+    }
+}
+
 template <int64_t blockSize, typename DstT, typename SrcT, typename BlockT, typename OffsetT, typename SrcCoord,
     typename OffsetCoord, typename BlockCoord>
 TILEOP void TGatherInL1(DstT dst, SrcT src, BlockT block, OffsetT offset, SrcCoord srcCoord, OffsetCoord offsetCoord,
@@ -681,21 +699,23 @@ TILEOP void TGatherInL1(DstT dst, SrcT src, BlockT block, OffsetT offset, SrcCoo
     uint64_t GMBlockTableOffset = GMBlockTableOffset0 * blockShape1 + GMBlockTableOffset1;
     constexpr auto staticL1H = Std::tuple_element<shapeSize - SHAPE_DIM2, typename DstT::TileShape>::type::value;
     constexpr auto staticL1W = Std::tuple_element<shapeSize - 1, typename DstT::TileShape>::type::value;
-    using shapeDim2 = pto::Shape<1, 1, 1, 1, -1>;
+    using shapeDim2 = pto::Shape<1, 1, 1, -1, -1>;
     using strideDim2 = pto::Stride<1, 1, 1, -1, -1>;
     using globalData = pto::GlobalTensor<typename SrcT::Type, shapeDim2, strideDim2, pto::Layout::ND>;
-    for (int64_t i = 0; i < dstShape0; i++) {
-        uint64_t gatherOffset = offset.GetAddr()[i + offsetsStartOffset];
-        gatherOffset = CalaOffset2PageAttention<uint64_t, typename BlockT::Type, blockSize>(
-            block.GetAddr() + GMBlockTableOffset, gatherOffset);
-        globalData src0Global(
-            (__gm__ typename SrcT::Type *)(src.GetAddr() + gatherOffset * srcShape1 + srcColumnStartOffset),
-            shapeDim2(staticL1W), strideDim2(srcStride0, srcStride1));
+    if (dstShape1 % c0Size > 0) {
         using tileData = pto::Tile<pto::TileType::Mat, typename DstT::Type, staticL1H, staticL1W,
             pto::BLayout::ColMajor, -1, -1, pto::SLayout::RowMajor>;
-        tileData dstL1(dstShape0, dstShape1);
-        pto::TASSIGN(dstL1, (uint64_t)((__cbuf__ typename DstT::Type *)dst.GetAddr() + i * c0Size));
-        pto::TLOAD(dstL1, src0Global);
+        GatherExecute<blockSize, globalData, tileData>(dst, src, block, offset, offsetsStartOffset, srcColumnStartOffset, GMBlockTableOffset,
+            1, staticL1W, srcStride0, srcStride1, dstShape0, dstShape1, srcShape1, dstShape0, c0Size);
+    } else {
+        using tileData = pto::Tile<pto::TileType::Mat, typename DstT::Type, staticL1W / c0Size, staticL1H * c0Size,
+            pto::BLayout::RowMajor, -1, -1>;
+        // 这里需要采用性能更高的ND2ND的搬运方式。
+        // GM上将(1, dstShape1)的数据，转变为(dstShape1 / c0Size, c0Size)。
+        // L1上将(staticL1W / c0Size, staticL1H / 16, 16, c0Size)的NZ数据，对每c0Size列做展平为一行，得到(staticL1W / c0Size, staticL1H * c0Size)的ND格式。
+        // 那么L1上有效数据就是(dstShape1 / c0Size, c0Size)。
+        GatherExecute<blockSize, globalData, tileData>(dst, src, block, offset, offsetsStartOffset, srcColumnStartOffset, GMBlockTableOffset,
+            dstShape1 / c0Size, c0Size, c0Size, 1, dstShape1 / c0Size, c0Size, srcShape1, dstShape0, c0Size);
     }
 }
 } // namespace TileOp
