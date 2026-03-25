c699b089dda7da98975f5dba67a45acadb4cb8ac
zhangxiangjie <zhangxiangjie5@huawei.com>
2026-03-25 11:43:13 +0800

    feat(pass): Add precheck for OP_INDEX_OUTCAST
    Co-authored-by: zhangxiangjie<zhangxiangjie5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1915 merge feat-add-check-0324 into 0.2.0
    
    feat(pass): Add precheck for OP_INDEX_OUTCAST
    
    Created-by: zhangxiangjie
    Commit-by: zhangxiangjie
    Merged-by: cann-robot
    Description: 1. 新增OP_INDEX_OUTCAST顺序校验，避免因无序导致对同一个input数据相互覆盖
    2. 调整assemble重叠校验时对动态offset的判定
    3. 调整部分pass的precheck为强制执行
    
    See merge request: cann/pypto!1915

--- framework/src/passes/pass_check/remove_redundant_reshape_checker.cpp
+++ framework/src/passes/pass_check/remove_redundant_reshape_checker.cpp
@@ -20,12 +20,17 @@
 
 namespace npu {
 namespace tile_fwk {
-Status RemoveRedundantReshapeChecker::DoPreCheck(Function &function) {
-    APASS_LOG_INFO_F(Elements::Operation, "PreCheck for RemoveRedundantShape.");
+Status RemoveRedundantReshapeChecker::DoDefaultEnabledPreCheck(Function &function) {
+    APASS_LOG_INFO_F(Elements::Function, "DoDefaultEnabledPreCheck for RemoveRedundantShape.");
     if (CheckValidOp(function) != SUCCESS) {
         APASS_LOG_ERROR_F(Elements::Operation, "Found invalid op from the function [%s].", function.GetRawName().c_str());
         return FAILED;
     }
+    return SUCCESS;
+}
+
+Status RemoveRedundantReshapeChecker::DoPreCheck(Function &function) {
+    APASS_LOG_INFO_F(Elements::Function, "PreCheck for RemoveRedundantShape.");
     if (CheckOpIOValid(function) != SUCCESS) {
         APASS_LOG_ERROR_F(Elements::Operation, "Found invalid input/output in the function [%s].", function.GetRawName().c_str());
         return FAILED;
@@ -44,7 +49,7 @@ Status RemoveRedundantReshapeChecker::DoPreCheck(Function &function) {
 }
 
 Status RemoveRedundantReshapeChecker::DoPostCheck(Function &function) {
-    APASS_LOG_INFO_F(Elements::Operation, "PostCheck for RemoveRedundantShape.");
+    APASS_LOG_INFO_F(Elements::Function, "PostCheck for RemoveRedundantShape.");
     for (const auto &op : function.Operations().DuplicatedOpList()) {
         if (ProcessPostCheck(*op)) {
             APASS_LOG_ERROR_F(Elements::Operation, "Postcheck RemoveRedundantShape failed. %s", GetFormatBacktrace(op).c_str());
