91948f1ba111e8343e6b81121e79d76e3219cedd
gguuaanngg <zhangguangjun2@huawei.com>
2026-06-05 18:28:32 +0800

    fix(operation): Modify operation doc
    Co-authored-by: gguuaanngg<zhangguangjun2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3804 merge master into master
    
    fix(operation): Modify operation doc
    
    Created-by: gguuaanngg
    Commit-by: gguuaanngg
    Merged-by: cann-robot
    Description: 1、Modify operation doc
    
    See merge request: cann/pypto!3804

--- framework/tests/cmake/scripts/helper/test_case_tools.py
+++ framework/tests/cmake/scripts/helper/test_case_tools.py
@@ -13,6 +13,7 @@ import math
 import pkgutil
 
 import numpy as np
+import torch
 
 
 def is_number(input_str: str):
@@ -80,8 +81,6 @@ def get_dtype_by_name(name: str, is_torch: bool = False, check: bool = True):
 
     if check and name == "bf16" and bfloat16 is None:
         raise TypeError("No module named 'ml_dtypes'.")
-
-    import torch
 
     str_to_dtype = {
         "int8": [np.int8, torch.int8],
