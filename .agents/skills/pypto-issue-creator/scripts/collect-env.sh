8a620813ec2fbaed534d65c3446ab37341f205f0
chengding888 <chengding1@hisilicon.com>
2026-06-08 21:02:08 +0800

    docs(scripts): Add copyright headers to shell scripts
    Co-authored-by: chengding888<chengding1@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3834 merge master into master
    
    docs(scripts): Add copyright headers to shell scripts
    
    Created-by: chengding888
    Commit-by: chengding888
    Merged-by: cann-robot
    Description: 脚本添加copyright
    
    See merge request: cann/pypto!3834

--- .agents/skills/pypto-issue-creator/scripts/collect-env.sh
+++ .agents/skills/pypto-issue-creator/scripts/collect-env.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
 # PyPTO 环境信息自动采集脚本
 # 用法: bash scripts/collect-env.sh
 # 输出: 格式化环境信息，可直接粘贴到 Bug Report 的 Environment 字段
