fix(operation): Add UNSUPPORT_FP16_ARCH35_OPS for fp16 MUL/MULS precision on A5
Co-authored-by: wkkk0528<wangkai578@huawei.com>



# message auto-generated for no-merge-commit merge:
!3768 merge fix_fp16_mul_arch35_precision into master

fix(operation): Add UNSUPPORT_FP16_ARCH35_OPS for fp16 MUL/MULS precision on A5

Created-by: wkkk0528
Commit-by: wkkk0528
Merged-by: cann-robot
Description: 修改pypto中add/mul/sub的bf16/fp16处理流程：
在A2A3/A5中，对于add/mul/sub的bf16/fp16场景，都进行升精度处理，以对标Ascendc的处理方式，解决三方比对fail的问题

See merge request: cann/pypto!3768

python/tests/st/interface/test_verify_frontend_jit.py
@@ -66,7 +66,7 @@ def test_verify_dyn():
     a = torch.rand(shape, dtype=torch.float16, device=device)
     b = torch.rand(shape, dtype=torch.float16, device=device)
     output_data = torch.zeros(shape, dtype=torch.float16, device=device)
-    golden = ((a * (a + b)) - b) * b
+    golden = (((a.float() * (a.float() + b.float())) - b.float()) * b.float()).half()
     pypto.set_verify_golden_data(goldens=[None, None, golden.cpu()])
     add_dyn_kernel(a, b, output_data)
     assert torch.allclose(output_data, golden)