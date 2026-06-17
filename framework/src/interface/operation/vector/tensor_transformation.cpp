b8cd1f0420dccec0c3f6638cca8d5aa4ed4e7d05
junnyleo <lijun580@huawei.com>
2026-06-17 14:12:42 +0800

    fix(operation): Fix remainder precision problem
    Co-authored-by: junnyleo<lijun580@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4096 merge fix/remainder into master
    
    fix(operation): Fix remainder precision problem
    
    Created-by: junnyleo
    Commit-by: junnyleo
    Merged-by: cann-robot
    Description: 修复remainder精度问题
    
    Related Issue: [#2315](https://gitcode.com/cann/pypto/issues/2315) 
    
    
    See merge request: cann/pypto!4096

--- framework/src/interface/operation/vector/tensor_transformation.cpp
+++ framework/src/interface/operation/vector/tensor_transformation.cpp
@@ -1517,7 +1517,9 @@ void TiledCastOperation(
             size_t shapeSize = input.tileInfo.shape.size();
             int64_t shapeW = (shapeSize >= 1) ? input.tileInfo.shape[shapeSize - 1] : 1;
             shapeW = AlignUp(shapeW + ALIGN_SIZE_64, static_cast<int64_t>(BLOCK_SIZE / BytesOf(DT_INT32)));
-            std::vector<int64_t> tmpShape = {1, shapeW};
+            int64_t shapeH = (shapeSize >= 2) ? input.tileInfo.shape[shapeSize - 2] : 1;
+            shapeH = std::min(shapeH, static_cast<int64_t>(MAX_REPEAT));
+            std::vector<int64_t> tmpShape = {shapeH, shapeW};
             auto tmpTensor = std::make_shared<LogicalTensor>(function, DT_INT32, tmpShape);
             op = &function.AddOperation(GetCastOpName<T>(), {tile}, {resultTile, tmpTensor});
         } else {
