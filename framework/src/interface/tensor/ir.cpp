405b222f18808347db9062d2a73e336ab600d2ac
W4termy <wangmingyang21@huawei.com>
2026-06-24 14:30:50 +0800

    feat(ir): Add fa test
    Co-authored-by: seu_chang<liuchang169@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3964 merge master into master
    
    feat(ir): Add fa test
    
    Created-by: seu_chang
    Commit-by: seu_chang
    Merged-by: cann-robot
    Description: fix(function): Move to outputs should be assemble
    fear(ir): Support repr for symbolic_scalar
    fear(ir): Add tokens for get_tensor_data/assemble
    fear(ir): Add binding api for pass
    feat(ir): Add aggresive_dce pass
    fix order of yield args maybe random
    refactor(ir): Add canonicalize pass
    fear(ir): Remove empty controlflow
    fear(ir): Support yield list/tuple args
    fear(ir): Support yield list/tuple args
    fear(ir): Support get_tensor_data for pil
    fear(ir): Make ctx as first arguments
    test(ir): Add fa test
    
    Related Issue: [#1185](https://gitcode.com/cann/pypto/issues/1185) 
    
    See merge request: cann/pypto!3964

--- framework/src/interface/tensor/ir.cpp
+++ framework/src/interface/tensor/ir.cpp
@@ -7,82 +7,24 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
+
 #include "ir.h"
-#include "ir/kind_traits.h"
-#include "ir/transforms/passes.h"
-#include "ir/transforms/utils/dead_code_elimination.h"
 
 #include "symbolic_scalar.h"
 #include "logical_tensor.h"
 
-using npu::tile_fwk::LogicalTensor;
-using npu::tile_fwk::RawTensor;
-using npu::tile_fwk::RawSymbolicExpression;
-
 namespace pypto::ir {
 std::string DumpScalarExpr(const ScalarExprPtr& op)
 {
-    auto p = std::dynamic_pointer_cast<const RawSymbolicExpression>(op);
+    auto p = std::dynamic_pointer_cast<const npu::tile_fwk::RawSymbolicExpression>(op);
     ASSERT(p) << "not a RawSymbolicExpression";
     return p->Dump();
 }
 
 std::string DumpTensorVar(const VarPtr& var)
 {
-    auto t = std::dynamic_pointer_cast<const LogicalTensor>(var);
+    auto t = std::dynamic_pointer_cast<const npu::tile_fwk::LogicalTensor>(var);
     ASSERT(t) << "not a logical tensor";
     return var->name_ + ": " + t->DumpType();
 }
-
-Pass pass::AggressiveDCE()
-{
-    return pass::CreateFunctionPass(
-        [](const FunctionPtr& func) -> FunctionPtr {
-            // Collect RawTensors from function input parameters.
-            std::unordered_set<RawTensor *> rawTensors;
-            for (const auto& param : func->params_) {
-                if (auto type = ir::As<LogicalTensorType>(param->GetType())) {
-                    auto t = std::dynamic_pointer_cast<const LogicalTensor>(param);
-                    rawTensors.insert(t->tensor.get());
-                }
-            }
-
-            auto isRemovable = [&rawTensors](const StmtPtr& stmt) -> bool {
-                // TensorOpStmt: removable unless OP_ASSEMBLE with matching input memref
-                if (auto tensorOp = std::dynamic_pointer_cast<const TensorOpStmt>(stmt)) {
-                    if (tensorOp->opcode_ != "ASSEMBLE")
-                        return true;
-                    auto ret = tensorOp->result_[0];
-                    auto tensor = std::dynamic_pointer_cast<const LogicalTensor>(ret);
-                    return rawTensors.count(tensor->tensor.get()) == 0;
-                }
-                if (auto assignOp = std::dynamic_pointer_cast<const AssignStmt>(stmt)) {
-                    return true;
-                }
-                if (auto scalarOp = std::dynamic_pointer_cast<const ScalarOpStmt>(stmt)) {
-                    return true;
-                }
-                // Everything else (ReturnStmt, YieldStmt, control flow, EvalStmt): not removable
-                return false;
-            };
-
-            auto newStmts = dce::EliminateDeadCode(func->body_->stmts_, isRemovable);
-            if (newStmts.size() == func->body_->stmts_.size()) {
-                bool changed = false;
-                for (size_t i = 0; i < newStmts.size(); ++i) {
-                    if (newStmts[i].get() != func->body_->stmts_[i].get()) {
-                        changed = true;
-                        break;
-                    }
-                }
-                if (!changed)
-                    return func;
-            }
-            auto newBody = std::make_shared<ir::SeqStmts>(std::move(newStmts), func->body_->span_);
-            return std::make_shared<ir::Function>(
-                func->name_, func->params_, func->returnTypes_, newBody, func->span_, func->funcType_);
-        },
-        "AggressiveDCE");
-}
-
 } // namespace pypto::ir
