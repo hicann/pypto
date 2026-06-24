49c5a51e99a3f8f0eec269ba41452ee2ee47f46c
cui_jiahao <cuijiahao5@h-partners.com>
2026-06-24 14:19:24 +0800

    refactor(codegen): Delete invoke param offset
    Co-authored-by: Bee.Hujun<Bee.hujun@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4177 merge codegen/delete_locOffset into master
    
    refactor(codegen): Delete invoke param offset
    
    Created-by: cam_bxh
    Commit-by: Bee.Hujun
    Merged-by: cann-robot
    Description: 1. Delete invoke param offset
    
    See merge request: cann/pypto!4177

--- framework/tests/ut/codegen/include/test_codegen_utils.h
+++ framework/tests/ut/codegen/include/test_codegen_utils.h
@@ -142,6 +142,7 @@ std::shared_ptr<LogicalTensor> CreateConvTensor(
     const bool& isCopyIn = true);
 
 struct GenOpCodeOptions {
+    std::map<int, int> lto = {};
     bool isMainBlk = false;
 };
 
