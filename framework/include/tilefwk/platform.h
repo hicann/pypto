a41d603f633bdf59b4bdf3a8cb27ecbfdf756b28
zyingshu <zhangyingshu1@h-partners.com>
2026-06-26 10:20:18 +0800

    feat(adapter/platform): Support kirinX90
    Co-authored-by: zyingshu<zhangyingshu1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4275 merge 0624_upstream_machine_etc into master
    
    feat(adapter/platform): Support kirinX90
    
    Created-by: zyingshu
    Commit-by: zyingshu
    Merged-by: cann-robot
    Description: Related Issue:
    [[Task|任务跟踪]: 增加KIRIN_X90平台](https://gitcode.com/cann/pypto/issues/2417)
    
    Support kirinX90
    
    See merge request: cann/pypto!4275

--- framework/include/tilefwk/platform.h
+++ framework/include/tilefwk/platform.h
@@ -39,6 +39,7 @@ enum class DPlatform {
     ASCEND_950PR_9582,
     ASCEND_950DT_9579,
     KIRIN_9030,
+    KIRIN_X90,
 
     UNKNOWN_DEVICE,
 };
@@ -46,10 +47,15 @@ enum class DPlatform {
 inline DPlatform StringToDpaltform(std::string platform)
 {
     std::unordered_map<std::string, DPlatform> mappings = {
-        {"ASCEND_910B1", DPlatform::ASCEND_910B1},           {"ASCEND_910B2", DPlatform::ASCEND_910B2},
-        {"ASCEND_910B3", DPlatform::ASCEND_910B3},           {"ASCEND_910B4", DPlatform::ASCEND_910B4},
-        {"ASCEND_950PR_9579", DPlatform::ASCEND_950PR_9579}, {"KIRIN_9030", DPlatform::KIRIN_9030},
-        {"ASCEND_950DT_9582", DPlatform::ASCEND_950DT_9582}, {"ASCEND_950PR_9582", DPlatform::ASCEND_950PR_9582},
+        {"ASCEND_910B1", DPlatform::ASCEND_910B1},
+        {"ASCEND_910B2", DPlatform::ASCEND_910B2},
+        {"ASCEND_910B3", DPlatform::ASCEND_910B3},
+        {"ASCEND_910B4", DPlatform::ASCEND_910B4},
+        {"ASCEND_950PR_9579", DPlatform::ASCEND_950PR_9579},
+        {"KIRIN_9030", DPlatform::KIRIN_9030},
+        {"KIRIN_X90", DPlatform::KIRIN_X90},
+        {"ASCEND_950DT_9582", DPlatform::ASCEND_950DT_9582},
+        {"ASCEND_950PR_9582", DPlatform::ASCEND_950PR_9582},
         {"ASCEND_950DT_9579", DPlatform::ASCEND_950DT_9579},
     };
 
