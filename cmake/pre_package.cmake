43441b3a1139b981556fa1017dcd18a56448ef9d
zhengfeiteng <zhengfeiteng1@h-partners.com>
2026-08-10 16:34:51 +0800

    feat(build): Support deb/rpm package in superbuild scene.
    Co-authored-by: Feiteng Zheng<zhengfeiteng1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5360 merge 20260728-intf-pub-2 into master
    
    feat(build): Support deb/rpm package in superbuild scene.
    
    Created-by: zhengfeiteng
    Commit-by: Feiteng Zheng
    Merged-by: cann-robot
    Description: 支持全量源码构建时，pypto编译deb/rpm包。
    
    - cpack生成deb/rpm包时，打包路径与External不同，cmake/pre_package.cmake中要分别处理。
    
    Related Issue: [#2573](https://gitcode.com/cann/pypto/issues/2573) 
    
    
    See merge request: cann/pypto!5360

--- cmake/pre_package.cmake
+++ cmake/pre_package.cmake
@@ -10,8 +10,14 @@
 
 get_filename_component(_PYPTO_DIR "${CMAKE_CURRENT_LIST_DIR}" DIRECTORY)
 
+if(CPACK_GENERATOR STREQUAL "External")
+    set(PYPTO_STAGING_DIR "${CPACK_CMAKE_BINARY_DIR}/_CPack_Packages/makeself_staging")
+else()
+    set(PYPTO_STAGING_DIR "${DEB_DELIVERY}")
+endif()
+
 execute_process(
-    COMMAND env PYPTO_UNIFIED_BUILD=1 PYPTO_STAGING_DIR=${CPACK_CMAKE_BINARY_DIR}/_CPack_Packages/makeself_staging python3 -m build --no-isolation --wheel --config-setting=--build-option=--plat-name=manylinux2014_${CMAKE_SYSTEM_PROCESSOR} -C=--build-option=--py-limited-api=cp37 --outdir ${CPACK_CMAKE_BINARY_DIR}/_CPack_Packages/makeself_staging/${CMAKE_SYSTEM_PROCESSOR}-linux/lib64
+    COMMAND env PYPTO_UNIFIED_BUILD=1 "PYPTO_STAGING_DIR=${PYPTO_STAGING_DIR}" python3 -m build --no-isolation --wheel --config-setting=--build-option=--plat-name=manylinux2014_${CMAKE_SYSTEM_PROCESSOR} -C=--build-option=--py-limited-api=cp37 --outdir ${CPACK_CMAKE_BINARY_DIR}/_CPack_Packages/makeself_staging/${CMAKE_SYSTEM_PROCESSOR}-linux/lib64
     WORKING_DIRECTORY ${_PYPTO_DIR}
     ERROR_VARIABLE error
     RESULT_VARIABLE ret
@@ -21,5 +27,5 @@ if(NOT ret EQUAL 0)
 endif()
 
 # 文件删除放在这里，支持重复调用whl打包
-file(REMOVE_RECURSE "${CPACK_CMAKE_BINARY_DIR}/_CPack_Packages/makeself_staging/lib")
-file(REMOVE_RECURSE "${CPACK_CMAKE_BINARY_DIR}/_CPack_Packages/makeself_staging/pypto")
+file(REMOVE_RECURSE "${PYPTO_STAGING_DIR}/lib")
+file(REMOVE_RECURSE "${PYPTO_STAGING_DIR}/pypto")
