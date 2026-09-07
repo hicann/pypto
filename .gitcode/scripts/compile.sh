de3d17fa2e1f91e2476356127aa732047d4f987d
wangyunxiang1 <wangyunxiang6@h-partners.com>
2026-09-07 14:51:34 +0800

    update pypto gcc15
    Co-authored-by: wangyunxiang<wangyunxiang6@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5816 merge master into master
    
    update pypto gcc15
    
    Created-by: wangyunxiang1
    Commit-by: wangyunxiang
    Merged-by: cann-robot
    Description: PR 应该避免没有描述信息，描述信息的最重要的作用是描述修改意图，应该清晰明了。例如：标题中包含了 `fix` 标签，如果没有描述信息，就需要Review人员猜测PR具体修复了哪些内容，或者怀疑修改的必要性。如果PR和特定的Issue相关，可以单独包含一行 `Related Issue:` 放到描述信息最后。
    
    下面是一个典型的PR描述信息:
    ```
    The origin issue template brings too many noise, it's not suitable for git message. eg: comments
    are not removed, labels are redundant as the PR tile already contains the tag.
    
    The guide gives a clear suggestion on how to write the PR body.
    
    Related Issue: #1234,#5678
    ```
    
    更多描述信息见：[Commit Message 规范](../docs/zh/contribute/pull-request.md#commit-message)
    
    
    See merge request: cann/pypto!5816

--- .gitcode/scripts/compile.sh
+++ .gitcode/scripts/compile.sh
@@ -14,12 +14,34 @@ fi
 echo $(grep -E "^VERSION_ID=" /etc/os-release | cut -d'"' -f2)
 export PATH=/opt/buildtools/python-3.10.2/bin:$PATH
 if [[ "${task_name}" == *ubuntu24* ]]; then
-    sudo update-alternatives --set gcc /usr/bin/gcc-14
+    if [ "${GIT_TARGET_BRANCH}" == "master" ]; then
+        sudo update-alternatives --set gcc /usr/bin/gcc-15
+    else
+        sudo update-alternatives --set gcc /usr/bin/gcc-14
+    fi
 else
     if [[ -f "/opt/rh/devtoolset-7/enable" ]]; then
         echo "source devtoolset"
         source /opt/rh/devtoolset-7/enable
     fi
+fi
+if gcc --version | head -n1 | grep -q "15\."; then
+    rm -rf /home/jenkins/opensource/lib_cache
+    if [ -d /home/jenkins/opensource/gcc15 ]; then
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15/lib_cache/ /home/jenkins/opensource/lib_cache
+    elif [ -d /home/jenkins/opensource/gcc15x86 ]; then
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15x86/lib_cache/ /home/jenkins/opensource/lib_cache
+    fi
+elif gcc --version | head -n1 | grep -q "14\."; then
+    gcc --version
+else
+    gcc --version
+    rm -rf /home/jenkins/opensource/lib_cache
+    ln -s /home/jenkins/opensource/ubuntu20/lib_cache /home/jenkins/opensource/lib_cache
 fi
 gcc --version
 
--- .gitcode/scripts/ut.sh
+++ .gitcode/scripts/ut.sh
@@ -3,7 +3,30 @@ set -euo pipefail
 
 export ASCEND_3RD_LIB_PATH=/home/jenkins/opensource
 export PATH=/opt/buildtools/python-3.10.2/bin:$PATH
-sudo update-alternatives --set gcc /usr/bin/gcc-14
+
+if [ "${GIT_TARGET_BRANCH}" == "master" ]; then
+    sudo update-alternatives --set gcc /usr/bin/gcc-15
+else
+    sudo update-alternatives --set gcc /usr/bin/gcc-14
+fi
+if gcc --version | head -n1 | grep -q "15\."; then
+    rm -rf /home/jenkins/opensource/lib_cache
+    if [ -d /home/jenkins/opensource/gcc15 ]; then
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15/lib_cache/ /home/jenkins/opensource/lib_cache
+    elif [ -d /home/jenkins/opensource/gcc15x86 ]; then
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15x86/lib_cache/ /home/jenkins/opensource/lib_cache
+    fi
+elif gcc --version | head -n1 | grep -q "14\."; then
+    gcc --version
+else
+    gcc --version
+    rm -rf /home/jenkins/opensource/lib_cache
+    ln -s /home/jenkins/opensource/ubuntu20/lib_cache /home/jenkins/opensource/lib_cache
+fi
 gcc --version
 rm -rf /home/jenkins/opensource/json
 
