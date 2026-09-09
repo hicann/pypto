f68c32eaf216659c3739908e47cde346274fb1ff
junnyleo <lijun580@huawei.com>
2026-09-09 16:32:32 +0800

    fix(operation): Remove int64/uint64's support for Gather/Full
    Co-authored-by: junnyleo<lijun580@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !6069 merge cherry-pick-mr-6039-1788915875297-auto into 9.2.0
    
    fix(operation): Remove int64/uint64's support for Gather/Full
    
    Created-by: junnyleo
    Commit-by: junnyleo
    Merged-by: cann-robot
    Description: 移除Gather,Full在A5上对DT_INT64、DT_UINT64的支持
    Related Issue: [#3261](https://gitcode.com/cann/pypto/issues/3261) 
    
    See merge request: cann/pypto!6069

--- framework/src/interface/operation/vector/gather.cpp
+++ framework/src/interface/operation/vector/gather.cpp
@@ -300,8 +300,8 @@ Tensor GatherElements(const Tensor& params, const Tensor& indices, int axis)
     }
     static const std::unordered_set<DataType> a2a3Types = {DT_FP32,   DT_FP16,  DT_BF16,  DT_INT32,
                                                            DT_UINT32, DT_INT16, DT_UINT16};
-    static const std::unordered_set<DataType> a5Types = {DT_FP32,  DT_FP16,   DT_BF16,  DT_INT32, DT_UINT32,
-                                                         DT_INT16, DT_UINT16, DT_INT64, DT_UINT64};
+    static const std::unordered_set<DataType> a5Types = {DT_FP32,   DT_FP16,  DT_BF16,  DT_INT32,
+                                                         DT_UINT32, DT_INT16, DT_UINT16};
     const auto& supportedTypes = GetSupportedDataTypesByArch(a2a3Types, a5Types);
     CheckTensorDataType(params.GetStorage(), supportedTypes, "GATHERELEMENTS");
     std::unordered_set<DataType> indexSupportedTypes = {DT_INT32, DT_INT64};
